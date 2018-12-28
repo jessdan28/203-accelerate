1ce6ec8b9694b543051500e2654af59e416172c1
Jess D <jessicadaniels28@gmail.com>
2018-12-28 07:14:34 -0500

    week two updates

--- wp-content/themes/accelerate-theme/footer.php
+++ wp-content/themes/accelerate-theme/footer.php
@@ -20,7 +20,7 @@
 				</div>
 
 				<nav class="social-media-navigation" role="navigation">
-					<?php wp_nav_menu( array( 'theme_location' => 'social-media', 'menu_class' => 'social-media-menu' ) ); ?>
+					<?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
 				</nav>
 			</div><!-- .site-info -->
 		</footer><!-- #colophon -->
